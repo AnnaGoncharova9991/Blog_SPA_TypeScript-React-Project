7f9090182cce2e8311baed045912d41f0f1708f7
Annasuper <annagoncharova9991@gmail.com>
2022-11-10 18:39:49 +0400

    feature: Added PersistLogin component

--- test-app/src/App.tsx
+++ test-app/src/App.tsx
@@ -1,27 +1,30 @@
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import BlogsPage from './pages/BlogsPage';
-import BlogPage from './pages/BlogPage';
-import ArticlesPage from './pages/ArticlesPage/Index';
-import NotFoundPage from './pages/NotFounPage';
-import RegistrationPage from './pages/RegistrationPage';
-import Layout from './components/Layout';
-import './App.scss'
-import ActivationPage from './pages/ActivationPage';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import BlogsPage from "./pages/BlogsPage";
+import BlogPage from "./pages/BlogPage";
+import ArticlesPage from "./pages/ArticlesPage/Index";
+import NotFoundPage from "./pages/NotFounPage";
+import RegistrationPage from "./pages/RegistrationPage";
+import Layout from "./components/Layout";
+import "./App.scss";
+import ActivationPage from "./pages/ActivationPage";
+import PersistLogin from "./components/PersistLogin";
 
 function App() {
   return (
     <Routes>
-      <Route element = {<Layout />}>
-        <Route path = '/' element = {<Navigate replace to = '/login'/>} />
-        <Route path = '/login' element = {<LoginPage/>} />
-        <Route path = '/registration' element = {<RegistrationPage/>} />
-        <Route path = '/blogs' element = {<BlogsPage/>} />
-        <Route path = '/articles' element = {<ArticlesPage/>} />
-        <Route path='/blog/:id' element ={<BlogPage />}/>
-        <Route path='/activate/:uid/:token' element ={<ActivationPage/>}/>
-        <Route path = '*' element = {<NotFoundPage/>} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Navigate replace to="/login" />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/registration" element={<RegistrationPage />} />
+        <Route path="/activate/:uid/:token" element={<ActivationPage />} />
+        <Route element={<PersistLogin />} >
+          <Route path="/blogs" element={<BlogsPage />} />
+          <Route path="/articles" element={<ArticlesPage />} />
+          <Route path="/blog/:id" element={<BlogPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
       </Route>
     </Routes>
   );
