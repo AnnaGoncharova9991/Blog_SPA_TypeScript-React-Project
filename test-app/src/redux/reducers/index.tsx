bea86b766dd800feb2ccb9a871157c6030bfedc1
Annasuper <annagoncharova9991@gmail.com>
2022-11-17 18:34:49 +0400

    feature: Added LOGOUT

--- test-app/src/redux/reducers/index.tsx
+++ test-app/src/redux/reducers/index.tsx
@@ -1,13 +1,19 @@
-import { combineReducers } from "redux";
+import { AnyAction, combineReducers } from "redux";
+import { LOGOUT } from "../actions/actions";
 import authReducer from "./authReducer";
 import blogsReducer from "./blogsReducer";
 
-const rootReducer = combineReducers( {
+const appReducer = combineReducers( {
     auth : authReducer,
     blogs : blogsReducer,
-    // articles : articlesReducer,
 });
 
-export type RootState = ReturnType<typeof rootReducer>;
+const rootReducer = (state: ReturnType<typeof appReducer> | undefined, action: AnyAction) => {
+    if (action.type === LOGOUT) {
+        localStorage.clear();
+        return appReducer({} as ReturnType<typeof appReducer> , action)
+    }
+    return appReducer(state, action);
+}
 
 export default rootReducer; 
