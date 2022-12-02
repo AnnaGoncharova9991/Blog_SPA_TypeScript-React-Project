c8d7bc88b7926601e3df3b0262a18d5015ac1f5f
Annasuper <annagoncharova9991@gmail.com>
2022-12-02 14:02:58 +0400

    feature: Added Switcher component

--- test-app/src/components/Layout/Footer/index.tsx
+++ test-app/src/components/Layout/Footer/index.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
                 <div className='container-footer'>
                     <div className='wrapper-footer'>
                         <p className='footer-text'> &copy;2022 Blogolog</p>
-                        <div className='switcher-wrapper'>
+                        <div className='switcher-btn-wrapper'>
                             <input className='switcher' id='switcher' type = 'checkbox' ></input>
                             <label className='switcher-tex' htmlFor='switcher'>Dark theme</label>                                        
                         </div>
